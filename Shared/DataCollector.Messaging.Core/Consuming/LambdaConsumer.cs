5005438ec41480c64adf04d640aa8672149ebea4
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-12-21 15:32:39 +0300

    base rpc

---
+++ Shared/DataCollector.Messaging.Core/Consuming/LambdaConsumer.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DataCollector.Messaging.Core.Consuming;
+
+public class LambdaConsumer<T> : IMessageConsumer<T> where T : class
+{
+    private readonly Func<ConsumeContext<T>, Task> _lambda;
+
+    public LambdaConsumer(Func<ConsumeContext<T>, Task> lambda)
+    {
+        _lambda = lambda;
+    }
+
+    public Task ConsumeAsync(ConsumeContext<T> context)
+    {
+        return _lambda.Invoke(context);
+    }
+}
---
+++ Shared/DataCollector.Messaging.Core/Rpc/RemoteFunction.cs
@@ -0,0 +1,49 @@
+﻿using DataCollector.Messaging.Core.Consuming;
+
+namespace DataCollector.Messaging.Core.Rpc;
+
+public class RemoteFunction<TArg, TResult> : IDisposable
+    where TArg : class
+    where TResult : class
+{
+    private readonly IMessageBroker _broker;
+
+    private IDisposable? _consumerHolder;
+    private TaskCompletionSource<TResult> _tcs;
+
+    public RemoteFunction(IMessageBroker broker)
+    {
+        _tcs = new TaskCompletionSource<TResult>();
+        _broker = broker;
+    }
+
+    public async Task<TResult> ExecuteRemoteAsync(TArg arg, CancellationToken cancellation)
+    {
+        if (_consumerHolder == null)
+            _consumerHolder = await CreateConsumerAsync();
+
+        await _broker.PublishAsync(arg);
+
+        var data = await _tcs.Task.WaitAsync(cancellation);
+        _tcs = new TaskCompletionSource<TResult>();
+
+        return data;
+    }
+
+    public void Dispose()
+    {
+        _consumerHolder?.Dispose();
+    }
+
+    private Task<IDisposable> CreateConsumerAsync()
+    {
+        var lambdaConsumer = new LambdaConsumer<TResult>(OnMessageRecived);
+        return _broker.SubscribeAsync(lambdaConsumer);
+    }
+
+    private Task OnMessageRecived(ConsumeContext<TResult> context)
+    {
+        _tcs.SetResult(context.Message);
+        return Task.CompletedTask;
+    }
+}

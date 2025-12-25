d09033f222332c1815d3283b369a3414bff65551
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2025-12-25 20:44:50 +0300

    user list colors

--- Services/Terminal/src/DataCollector.Terminal.App/Controls/UsersList.xaml.cs
+++ Services/Terminal/src/DataCollector.Terminal.App/Controls/UsersList.xaml.cs
@@ -1,11 +1,12 @@
 using DataCollector.Terminal.App.DTOs;
+using System.Collections;
 
 namespace DataCollector.Terminal.App.Controls;
 
 public partial class UsersList : ContentView
 {
-    public static readonly BindableProperty UsersSourceProperty =
-        BindableProperty.Create(nameof(Users), typeof(IEnumerable<UserDTO>), 
+    public static readonly BindableProperty UsersProperty =
+        BindableProperty.Create(nameof(Users), typeof(IEnumerable), 
             typeof(UsersList), Enumerable.Empty<UserDTO>(), BindingMode.OneWay);
 
     public static readonly BindableProperty SelectedUsersProperty =
@@ -14,9 +15,9 @@ public partial class UsersList : ContentView
 
     public IEnumerable<UserDTO> Users
     {
-        get => (IEnumerable<UserDTO>)GetValue(UsersSourceProperty);
+        get => (IEnumerable<UserDTO>)GetValue(UsersProperty);
 
-        set => SetValue(UsersSourceProperty, value);
+        set => SetValue(UsersProperty, value);
     }
 
     public IEnumerable<UserDTO> SelectedUsers => (IEnumerable<UserDTO>)GetValue(SelectedUsersProperty);
---
+++ Services/Terminal/src/DataCollector.Terminal.App/Converters/RoleColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using DataCollector.Domain;
+using System.Globalization;
+
+namespace DataCollector.Terminal.App.Converters;
+
+public class RoleColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not UserRoles role)
+            return Binding.DoNothing;
+
+        return role switch
+        {
+            UserRoles.Publisher => Colors.Yellow,
+            UserRoles.Admin => Colors.Red,
+            _ => Colors.White
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
